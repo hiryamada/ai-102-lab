356195bd560372c4abbdc334386bb05d31c14329
hiryamada <71864722+hiryamada@users.noreply.github.com>
2025-05-15 22:32:10 +0900

    update

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 
+- ラボを起動
+- ラボ内部のWindowsスタートボタンを右クリックし「Windows ターミナル（管理者）」をクリック
+- 「このアプリがデバイスに変更を加えることを許可しますか？」→「はい」
+- 以下をコピーし、ターミナルに貼り付けてエンター
 
 ```pwsh
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/hiryamada/ai-102-lab/refs/heads/main/install.ps1'))
--- install.ps1
+++ install.ps1
@@ -1,12 +1,13 @@
 Install-PackageProvider -Name NuGet -Force | Out-Null
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
 Repair-WinGetPackageManager -Force -Latest
-winget install -e -h --accept-package-agreements --accept-source-agreements --force --id Git.Git
-winget install -e -h --accept-package-agreements --accept-source-agreements --force --id Microsoft.VisualStudioCode
-winget install -e -h --accept-package-agreements --accept-source-agreements --force --id Microsoft.DotNet.SDK.9
-winget install -e -h --accept-package-agreements --accept-source-agreements --force --id Microsoft.PowerShell
-winget install -e -h --accept-package-agreements --accept-source-agreements --force --id Microsoft.AzureCLI
-code --install-extension ms-dotnettools.csharp
-code --install-extension ms-dotnettools.csdevkit
-code --install-extension ms-dotnettools.dotnet-interactive-vscode
-code
+winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --id Git.Git
+winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --scope machine --id Microsoft.VisualStudioCode
+winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --id Microsoft.DotNet.SDK.9
+winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --id Microsoft.PowerShell
+winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --id Microsoft.AzureCLI
+code --install-extension --force ms-dotnettools.csharp
+code --install-extension --force ms-dotnettools.csdevkit
+code --install-extension --force ms-dotnettools.dotnet-interactive-vscode
+git clone https://github.com/hiryamada/ai-102-lab
+code ai-102-lab
