93024f5b3ac968f2cd00f929fa2c4890157ec6b8
hiryamada <71864722+hiryamada@users.noreply.github.com>
2025-05-15 23:02:00 +0900

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,11 +1,11 @@
 Install-PackageProvider -Name NuGet -Force | Out-Null
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
 Repair-WinGetPackageManager -Force -Latest
-winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --id Git.Git
-winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --scope machine --id Microsoft.VisualStudioCode
+#winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --id Git.Git
+#winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --scope machine --id Microsoft.VisualStudioCode
 winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --id Microsoft.DotNet.SDK.9
-winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --id Microsoft.PowerShell
-winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --id Microsoft.AzureCLI
+#winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --id Microsoft.PowerShell
+#winget install -e -h --accept-package-agreements --accept-source-agreements --no-upgrade --id Microsoft.AzureCLI
 code --install-extension --force ms-dotnettools.csharp
 code --install-extension --force ms-dotnettools.csdevkit
 code --install-extension --force ms-dotnettools.dotnet-interactive-vscode
