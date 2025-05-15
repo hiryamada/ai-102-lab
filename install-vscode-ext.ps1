04fc7c6114e6261cd77cd475550a55c334d0f078
hiryamada <71864722+hiryamada@users.noreply.github.com>
2025-05-15 23:33:57 +0900

    Create install-vscode-ext.ps1

---
+++ install-vscode-ext.ps1
@@ -0,0 +1,3 @@
+code --install-extension ms-dotnettools.csharp --force
+code --install-extension ms-dotnettools.csdevkit --force
+code --install-extension ms-dotnettools.dotnet-interactive-vscode --force
